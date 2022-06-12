0f30a4f3a3d3e24af2c031843a8b551c110e2903
umutekinkarasah <72706013+umutekinkarasah@users.noreply.github.com>
2022-06-12 19:50:24 +0300

    Add files via upload

---
+++ time_cal/time_cal.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int time_spend()
+{
+	int day, hour, min, sec;
+	int temp1, temp2;
+	
+	
+	printf("\n\n\n\n\t\t\t\t\t\t\t\t\tLutfen seyehatinizin kac gun, saat, dakika ve saniye oldugunu giriniz.");
+	printf("\n\n\t\t\t\t\t\t\t\t\t\t\t\tLutfen gun bilgisini giriniz : ");
+	scanf("%d", &day);
+	printf("\n\n\t\t\t\t\t\t\t\t\t\t\t\tLutfen saat bilgisini giriniz : ");
+	scanf("%d", &hour);
+	printf("\n\n\t\t\t\t\t\t\t\t\t\t\t\tLutfen dakika bilgisini giriniz : ");
+	scanf("%d", &min);
+	printf("\n\n\t\t\t\t\t\t\t\t\t\t\t\tLutfen saniye bilgisini giriniz : ");
+	scanf("%d", &sec);
+	
+	
+	if (day < 0 || hour < 0 || min < 0 || sec < 0)
+		printf("\n\n\t\t\t\t\t\t\t\t\t\t\t\tHatali giris, sure degeri eski olamaz.\n\n\n\n\n\n");
+	
+	else
+	{
+	
+	if(sec > 60)
+	{	
+		min += (sec / 60);	// kac ker bölündügü
+		sec = sec % 60; 	// artan		
+	}
+
+	if(min > 60)
+	{	
+		hour += (min / 60);
+		min %= 60;
+	}	
+
+	if(hour > 24)
+	{	
+		day += (hour / 24);
+		hour = hour % 24;
+	
+	}
+
+	printf("\n\n\t\t\t\t\t\t\t\t\t\tSeyahatiniz %d gun, %d saat, %d dakika ve %d saniye surmustur.\n\n\n\n", day, hour, min, sec);
+
+    }
+}
+
+
+
+
+int main(){
+	
+	time_spend();
+	return 0;
+	
+}
